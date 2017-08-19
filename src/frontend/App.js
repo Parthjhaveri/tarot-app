17e5f5c5a61c857274e054cf3d68d27f96180b2f
Parthjhaveri <parthjhaveri209@gmail.com>
2017-08-19 01:35:12 -0400

    setup SASS and file structure

---
+++ src/frontend/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles/sass/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        App!
+      </div>
+    );
+  }
+}
+
+export default App;

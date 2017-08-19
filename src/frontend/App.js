38c0395f0fc7be617bd94a8d2ca230610d81f76a
Parthjhaveri <parthjhaveri209@gmail.com>
2017-08-19 02:41:59 -0400

    initial styles

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import MainBanner from './components/mainBanner.js';
+import Navbar from './components/navbar.js';
 import './styles/sass/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <MainBanner />
       </div>
     );
---
+++ src/frontend/components/navbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/sass/App.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className="navDiv">
+        <button className="contactMe">CONTACT</button>
+      </div>
+    );
+  }
+}
+
+export default Navbar;

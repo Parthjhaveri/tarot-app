16f441209b250e9081147d2e0c4c16a43b47237c
Parthjhaveri <parthjhaveri209@gmail.com>
2017-08-20 17:32:49 -0400

    styling changes/added dummy picture

--- src/frontend/components/waysToBook.js
+++ src/frontend/components/waysToBook.js
@@ -6,6 +6,33 @@ class BookMe extends Component {
     return (
       <div className="bookMeDiv">
 
+      <center><h1>2 types of appointments:</h1></center>
+      <br />
+      <br />
+
+      <center>
+        <div className="row">
+
+          <div className="col-md-6">
+            <div className="bookBox">
+              <center><p><i className="fa fa-user" id="userIcon" aria-hidden="true"></i></p></center>
+              <br />
+              <h2>In person</h2>
+              <center><button className="schedApptButton">Schedule appointment</button></center>
+            </div>
+          </div>
+
+          <div className="col-md-6">
+            <div className="bookBox">
+              <center><p><i className="fa fa-mobile" id="phoneIcon" aria-hidden="true"></i></p></center>
+              <br />
+              <h2>Over the phone</h2>
+              <center><button className="schedApptButton">Schedule appointment</button></center>
+            </div>
+          </div>
+        </div>
+      </center>
+
       </div>
     );
   }
